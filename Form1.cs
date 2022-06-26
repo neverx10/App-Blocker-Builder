497a63dc95fca254686dcd67129741b407e30528
NeverX <105104238+neverx10@users.noreply.github.com>
2022-06-26 12:17:22 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace App_Blocker_Builder_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = ("By NeverX");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string[] str = new string[]
+         {
+               "@echo off",
+               ":main",
+               "taskkill /pid " + Process.Text,
+               "goto:main",
+               
+         };
+
+            using (StreamWriter sw = new StreamWriter("OutPut.bat"))
+            {
+                foreach (string s in str)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+
+            // Lire et afficher chaque ligne du fichier.
+            string line = "";
+            using (StreamReader sr = new StreamReader("OutPut.bat"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+ 
+}
